4e1bdf4fddaa63cbd433058adf682d665d4d2aef
TDeMaria64 <tdemaria64@gmail.com>
2022-09-23 11:42:57 -0400

    Add project files.

---
+++ PriceQuotation/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuotation.Models;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace PriceQuotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Subtotal:";
+            ViewBag.FV1 = 0.00;
+            ViewBag.FV2 = 0.00;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Quotation model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV1 = model.CalculateDiscountAmount();
+                ViewBag.FV2 = model.CalculateTotal();
+            }
+            else
+            {
+                ViewBag.FV1 = 0;
+                ViewBag.FV2 = 0;
+            }
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PriceQuotation/Models/Quotation.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotation.Models
+{
+    public class Quotation
+    {
+        [Required(ErrorMessage =
+           "Please enter a subtotal.")]
+        [Range(1, double.MaxValue, ErrorMessage =
+            "Subtotal must be greater than 0.")]
+        public decimal? Subtotal { get; set; }
+        [Required(ErrorMessage =
+            "Please enter a discount percent.")]
+        [Range(0, 100, ErrorMessage =
+            "Discount percent must be between 0 and 100.")]
+        public decimal? DiscountPercent { get; set; }
+
+        public decimal? CalculateDiscountAmount()
+        {
+            decimal? percent = DiscountPercent / 100;
+            decimal? discountamount = percent * Subtotal;
+            return discountamount;
+        }
+
+        public decimal? CalculateTotal()
+        {
+            decimal? total = Subtotal - CalculateDiscountAmount();
+            return total;
+        }
+    }
+}
